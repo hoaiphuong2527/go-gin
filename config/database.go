0fcbe9417ff61bc4070567e636876eb30d429a6e
Hoai Phuong <hoaiphuong2527@gmail.com>
2025-03-20 17:06:29 +0700

    feat: config env

--- config/database.go
+++ config/database.go
@@ -2,25 +2,38 @@ package config
 
 import (
 	"fmt"
-	"go-gin-framework/models"
 	"log"
 
 	"gorm.io/driver/mysql"
+	// "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "root:@tcp(127.0.0.1:3306)/gin?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Cannot connect to MySQL:", err)
+	LoadEnv()
+
+	dbDriver := GetEnv("DB_DRIVER", "mysql")
+	dbUser := GetEnv("DB_USER", "root")
+	dbPassword := GetEnv("DB_PASSWORD", "")
+	dbHost := GetEnv("DB_HOST", "localhost")
+	dbPort := GetEnv("DB_PORT", "3306")
+	dbName := GetEnv("DB_NAME", "mydatabase")
+	var err error
+	if dbDriver == "mysql" {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			dbUser, dbPassword, dbHost, dbPort, dbName)
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	} else if dbDriver == "postgres" {
+		// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		// 	dbHost, dbUser, dbPassword, dbName, dbPort)
+		// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	}
 
-	fmt.Println("Connected to MySQL")
-	DB = database
+	if err != nil {
+		log.Fatal("🚀 Failed to connect to database:", err)
+	}
 
-	// Tạo bảng nếu chưa có
-	database.AutoMigrate(&models.User{})
+	log.Println("✅ Connected to the database successfully!")
 }
--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"go-gin-framework/config"
 	"go-gin-framework/routes"
+	"log"
 )
 
 func main() {
 	config.ConnectDatabase()
-
+	config.LoadEnv()
+	port := config.GetEnv("SERVER_PORT", "8080")
 	r := routes.SetupRouter()
-	r.Run(":8080")
+	r.Run(":" + port)
+	err := r.Run(":" + port)
+	log.Println("🚀 Server is running on port:", port)
+	if err != nil {
+		log.Fatal("❌ Failed to start server:", err)
+	}
 }
